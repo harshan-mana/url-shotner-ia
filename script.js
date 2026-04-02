6e2ca4395d020e284730375447ce7d8f6a56dd13
harshan-mana <harshan.23ise@cambridge.edu.in>
2026-04-02 11:43:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+async function shortenMultiple() {
+    const longUrl = document.getElementById('longUrl').value;
+    const container = document.getElementById('results-container');
+    if (!longUrl) return alert("Enter a URL");
+    container.innerHTML = "Shortening...";
+    const apis = [
+        `https://tinyurl.com/api-create.php?url=${encodeURIComponent(longUrl)}`,
+        `https://is.gd/create.php?format=simple&url=${encodeURIComponent(longUrl)}`,
+        `https://v.gd/create.php?format=simple&url=${encodeURIComponent(longUrl)}`
+    ];
+    try {
+        const results = await Promise.all(apis.map(api => fetch(api).then(res => res.text())));
+        container.innerHTML = ""; 
+        
+        results.forEach(link => {
+            const div = document.createElement('div');
+            div.className = "result-item";
+            div.innerText = link;
+            container.appendChild(div);
+        });
+    } catch (e) { container.innerHTML = "Error!"; }
+}
 
